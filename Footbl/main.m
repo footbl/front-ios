4f6ca43247e8c91c4336e74f956622e7098c16dc
Fernando Saragoça <fsaragoca@me.com>
2014-03-25 14:24:55 -0300

    Initial commit

---
+++ Footbl/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Footbl
+//
+//  Created by Fernando Saragoça on 3/25/14.
+//  Copyright (c) 2014 made@sampa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FootblTests/FootblTests.m
@@ -0,0 +1,31 @@
+//
+//  FootblTests.m
+//  FootblTests
+//
+//  Created by Fernando Saragoça on 3/25/14.
+//  Copyright (c) 2014 made@sampa. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FootblTests : XCTestCase
+
+@end
+
+@implementation FootblTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
