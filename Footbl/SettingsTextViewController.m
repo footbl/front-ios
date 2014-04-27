17474d022ea9320f99d27aaeb0db48e3f672cb57
Fernando Saragoça <fsaragoca@me.com>
2014-04-27 20:17:12 -0300

    Disables editing on settings text view

--- Footbl/SettingsTextViewController.m
+++ Footbl/SettingsTextViewController.m
@@ -39,6 +39,7 @@
     
     self.textView = [[UITextView alloc] initWithFrame:self.view.bounds];
     self.textView.text = self.text;
+    self.textView.editable = NO;
     [self.view addSubview:self.textView];
 }
 
