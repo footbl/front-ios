688c4cb1e51e6e2785e430fdf62606888d643a27
Fernando Saragoça <fsaragoca@me.com>
2014-04-27 19:32:31 -0300

    Updates SPHipster podspec to 0.3

--- Pods/SPHipster/SPHipster/SPLog.h
+++ Pods/SPHipster/SPHipster/SPLog.h
@@ -14,8 +14,10 @@ typedef NS_ENUM(NSInteger, SPDebugLogLevel) {
 };
 extern SPDebugLogLevel kSPDebugLogLevel; // defaults to SPLogLevelError
 
-#define SPLog(x, ...) if(kSPDebugLogLevel >= SPDebugLogLevelInfo) NSLog(@"%@: " x, SPGenerateLogString(__FILE__, __LINE__, __FUNCTION__), ##__VA_ARGS__)
-#define SPLogError(x, ...) if(kSPDebugLogLevel >= SPDebugLogLevelError) NSLog(@"Error: %@ " x, SPGenerateLogString(__FILE__, __LINE__, __FUNCTION__), ##__VA_ARGS__)
-#define SPLogVerbose(x, ...) if(kSPDebugLogLevel >= SPDebugLogLevelVerbose) NSLog(@"%@: " x, SPGenerateLogString(__FILE__, __LINE__, __FUNCTION__), ##__VA_ARGS__)
+#define SPLog(x, ...) if(kSPDebugLogLevel >= SPDebugLogLevelInfo) NSLog(@"%@: " x, SPGenerateLogString(__FILE__, __LINE__, __FUNCTION__, SPDebugLogLevelInfo), ##__VA_ARGS__)
+#define SPLogError(x, ...) if(kSPDebugLogLevel >= SPDebugLogLevelError) NSLog(@"%@: " x, SPGenerateLogString(__FILE__, __LINE__, __FUNCTION__, SPDebugLogLevelError), ##__VA_ARGS__)
+#define SPLogVerbose(x, ...) if(kSPDebugLogLevel >= SPDebugLogLevelVerbose) NSLog(@"%@: " x, SPGenerateLogString(__FILE__, __LINE__, __FUNCTION__, SPDebugLogLevelVerbose), ##__VA_ARGS__)
 
-extern NSString * SPGenerateLogString(const char *file, int lineNumber, const char *functionName);
+extern NSString * SPGenerateLogString(const char *file, int lineNumber, const char *functionName, SPDebugLogLevel logLevel);
+extern NSString * SPLogFilePath();
+extern void SPLogSwitchToLocalFiles();
--- Pods/SPHipster/SPHipster/SPLog.m
+++ Pods/SPHipster/SPHipster/SPLog.m
@@ -14,7 +14,35 @@
 
 SPDebugLogLevel kSPDebugLogLevel = SPDebugLogLevelError;
 
-NSString * SPGenerateLogString(const char *file, int lineNumber, const char *functionName) {
+NSString * SPLogFilePath()  {
+    NSString *folder = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES)[0] stringByAppendingPathComponent:@"Logs"];
+    [[NSFileManager defaultManager] createDirectoryAtPath:folder withIntermediateDirectories:YES attributes:nil error:nil];
+    return folder;
+}
+
+void SPLogSwitchToLocalFiles() {
+    NSString *path = SPLogFilePath();
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    formatter.dateFormat = @"yyyy-MM-dd HH'h'mm'm'ss's'";
+    path = [[path stringByAppendingPathComponent:[formatter stringFromDate:[NSDate date]]] stringByAppendingPathExtension:@"log"];
+    [@"" writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:path];
+    dup2([fileHandle fileDescriptor], STDERR_FILENO);
+}
+
+NSString * SPGenerateLogString(const char *file, int lineNumber, const char *functionName, SPDebugLogLevel logLevel) {
     NSString *fileName = [[NSString stringWithUTF8String:file] lastPathComponent];
-    return [NSString stringWithFormat:@"(%s) (%@:%i)", functionName, fileName, lineNumber];
+    NSString *logType;
+    switch (logLevel) {
+        case SPDebugLogLevelVerbose:
+            logType = @"VERBOSE";
+            break;
+        case SPDebugLogLevelError:
+            logType = @"ERROR";
+            break;
+        default:
+            logType = @"INFO";
+            break;
+    }
+    return [NSString stringWithFormat:@"%@ (%s) (%@:%i)", logType, functionName, fileName, lineNumber];
 }
