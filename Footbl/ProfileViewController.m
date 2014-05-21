20133b45ffece119c2a95373b0b08c02736623b6
Fernando Saragoça <fsaragoca@me.com>
2014-05-21 01:34:56 -0300

    Adds bets to profile view controller

--- Footbl/ProfileViewController.m
+++ Footbl/ProfileViewController.m
@@ -7,12 +7,18 @@
 //
 
 #import <SDWebImage/UIImageView+WebCache.h>
+#import "Bet.h"
 #import "Championship.h"
 #import "FootblAPI.h"
+#import "Match.h"
+#import "MatchTableViewCell.h"
 #import "ProfileChampionshipTableViewCell.h"
 #import "ProfileTableViewCell.h"
 #import "ProfileViewController.h"
 #import "SettingsViewController.h"
+#import "Team.h"
+#import "TeamImageView.h"
+#import "UILabel+MaxFontSize.h"
 #import "User.h"
 #import "Wallet.h"
 #import "WalletHighestTableViewCell.h"
@@ -24,6 +30,7 @@
 @property (strong, nonatomic) NSNumber *totalWallet;
 @property (strong, nonatomic) NSNumber *numberOfWallets;
 @property (strong, nonatomic) NSArray *championships;
+@property (strong, nonatomic) NSArray *bets;
 
 @end
 
@@ -75,6 +82,13 @@
     self.numberOfWallets = @(fetchResult.count);
     self.totalWallet = [fetchResult valueForKeyPath:@"@sum.funds"];
     self.championships = [[fetchResult valueForKeyPath:@"championship"] sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"edition" ascending:NO], [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES], [NSSortDescriptor sortDescriptorWithKey:@"rid" ascending:YES]]];
+    
+    fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Bet"];
+    fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"match.date" ascending:NO], [NSSortDescriptor sortDescriptorWithKey:@"match.rid" ascending:YES]];
+    fetchResult = [FootblManagedObjectContext() executeFetchRequest:fetchRequest error:&error];
+    self.bets = fetchResult;
+    
+    [self.tableView reloadData];
     
     [self.user updateWithSuccess:^{
         [self.tableView reloadData];
@@ -129,6 +143,103 @@
 #warning Add ranking text
             break;
         }
+        case 2: {
+            Match *match = [self.bets[indexPath.row] match];
+            MatchTableViewCell *matchCell = (MatchTableViewCell *)cell;
+            matchCell.hostNameLabel.text = match.host.displayName;
+            matchCell.hostPotLabel.text = @"0";
+            [matchCell.hostImageView setImageWithURL:[NSURL URLWithString:match.host.picture]];
+            [matchCell.hostDisabledImageView setImageWithURL:[NSURL URLWithString:match.host.picture]];
+            matchCell.drawPotLabel.text = @"0";
+            matchCell.guestNameLabel.text = match.guest.displayName;
+            [matchCell.guestImageView setImageWithURL:[NSURL URLWithString:match.guest.picture]];
+            [matchCell.guestDisabledImageView setImageWithURL:[NSURL URLWithString:match.guest.picture]];
+            matchCell.guestPotLabel.text = @"0";
+            matchCell.hostScoreLabel.text = match.hostScore.stringValue;
+            matchCell.guestScoreLabel.text = match.guestScore.stringValue;
+            
+            CGFloat potTotal = match.potHostValue + match.potGuestValue + match.potDrawValue;
+            
+            NSNumberFormatter *numberFormatter = [NSNumberFormatter new];
+            numberFormatter.maximumFractionDigits = 2;
+            numberFormatter.minimumFractionDigits = 0;
+            
+            if (match.potHostValue > 0) {
+                matchCell.hostPotLabel.text = [numberFormatter stringFromNumber:@(potTotal / match.potHostValue)];
+            }
+            if (match.potDrawValue > 0) {
+                matchCell.drawPotLabel.text = [numberFormatter stringFromNumber:@(potTotal / match.potDrawValue)];
+            }
+            if (match.potGuestValue > 0) {
+                matchCell.guestPotLabel.text = [numberFormatter stringFromNumber:@(potTotal / match.potGuestValue)];
+            }
+            
+            // Auto-decrease font size to fit bounds
+            matchCell.hostNameLabel.font = [UIFont fontWithName:matchCell.hostNameLabel.font.fontName size:matchCell.defaultTeamNameFontSize];
+            matchCell.guestNameLabel.font = [UIFont fontWithName:matchCell.guestNameLabel.font.fontName size:matchCell.defaultTeamNameFontSize];
+            matchCell.drawLabel.font = [UIFont fontWithName:matchCell.drawLabel.font.fontName size:matchCell.defaultTeamNameFontSize];
+            CGFloat maxHostNameSize = matchCell.hostNameLabel.maxFontSizeToFitBounds;
+            CGFloat maxGuestNameSize = matchCell.guestNameLabel.maxFontSizeToFitBounds;
+            CGFloat maxFontSize = MIN(maxHostNameSize, maxGuestNameSize);
+            matchCell.hostNameLabel.font = [UIFont fontWithName:matchCell.hostNameLabel.font.fontName size:maxFontSize];
+            matchCell.guestNameLabel.font = [UIFont fontWithName:matchCell.guestNameLabel.font.fontName size:maxFontSize];
+            matchCell.drawLabel.font = [UIFont fontWithName:matchCell.drawLabel.font.fontName size:maxFontSize];
+            
+            NSDateFormatter *formatter = [NSDateFormatter new];
+            formatter.dateStyle = NSDateFormatterShortStyle;
+            formatter.timeStyle = NSDateFormatterShortStyle;
+            formatter.AMSymbol = @"am";
+            formatter.PMSymbol = @"pm";
+            formatter.dateFormat = [@"EEEE, " stringByAppendingString:formatter.dateFormat];
+            formatter.dateFormat = [formatter.dateFormat stringByReplacingOccurrencesOfString:@", y" withString:@""];
+            formatter.dateFormat = [formatter.dateFormat stringByReplacingOccurrencesOfString:@"/y" withString:@""];
+            formatter.dateFormat = [formatter.dateFormat stringByReplacingOccurrencesOfString:@"y" withString:@""];
+            [matchCell setDateText:[formatter stringFromDate:match.date]];
+            
+            MatchResult result = (MatchResult)match.bet.resultValue;
+            if (match.tempBetValue) {
+                result = match.tempBetResult;
+            }
+            
+            switch (result) {
+                case MatchResultHost:
+                    matchCell.layout = MatchTableViewCellLayoutHost;
+                    break;
+                case MatchResultGuest:
+                    matchCell.layout = MatchTableViewCellLayoutGuest;
+                    break;
+                case MatchResultDraw:
+                    matchCell.layout = MatchTableViewCellLayoutDraw;
+                    break;
+                default:
+                    matchCell.layout = MatchTableViewCellLayoutNoBet;
+                    break;
+            }
+            
+            matchCell.stakeValueLabel.text = match.bet.value.stringValue;
+            matchCell.returnValueLabel.text = @"-";
+            matchCell.profitValueLabel.text = @"-";
+            
+            if (match.elapsed) {
+                matchCell.liveLabel.text = [NSString stringWithFormat:NSLocalizedString(@"Live - %i'", @"Live - {time elapsed}'"), match.elapsed.integerValue];
+                matchCell.stateLayout = MatchTableViewCellStateLayoutLive;
+            } else if (match.finishedValue) {
+                matchCell.liveLabel.text = NSLocalizedString(@"Final", @"");
+                matchCell.stateLayout = MatchTableViewCellStateLayoutDone;
+            } else {
+                matchCell.liveLabel.text = @"";
+                matchCell.stateLayout = MatchTableViewCellStateLayoutWaiting;
+            }
+            
+            // Just for testing
+            [matchCell.hostImageView setImageWithURL:[NSURL URLWithString:@"https://dl.dropboxusercontent.com/u/6954324/Aplicativos/Footbl/Temp/Escudo_COR%402x.png"]];
+            [matchCell.hostDisabledImageView setImageWithURL:[NSURL URLWithString:@"https://dl.dropboxusercontent.com/u/6954324/Aplicativos/Footbl/Temp/Escudo_COR%402x.png"]];
+            [matchCell.guestImageView setImageWithURL:[NSURL URLWithString:@"https://dl.dropboxusercontent.com/u/6954324/Aplicativos/Footbl/Temp/Escudo_SAN%402x.png"]];
+            [matchCell.guestDisabledImageView setImageWithURL:[NSURL URLWithString:@"https://dl.dropboxusercontent.com/u/6954324/Aplicativos/Footbl/Temp/Escudo_SAN%402x.png"]];
+            [matchCell setStakesCount:@143 commentsCount:@48];
+            
+            break;
+        }
         default:
             break;
     }
@@ -149,6 +260,10 @@
         [view addSubview:separatorView];
         
         return view;
+    } else if (section == 2 && self.bets.count > 0) {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(tableView.frame), 7)];
+        view.backgroundColor = [UIColor clearColor];
+        return view;
     } else {
         return nil;
     }
@@ -157,13 +272,15 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     if (section == 1 && self.championships.count > 0) {
         return 10;
+    } else if (section == 2 && self.bets.count > 0) {
+        return 7;
     } else {
         return 0;
     }
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 2;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
@@ -172,6 +289,8 @@
             return 3;
         case 1:
             return self.championships.count;
+        case 2:
+            return self.bets.count;
         default:
             return 1;
     }
@@ -197,6 +316,9 @@
             break;
         case 1:
             identifier = @"ChampionshipCell";
+            break;
+        case 2:
+            identifier = @"MatchCell";
             break;
         default:
             break;
@@ -225,6 +347,14 @@
             break;
         case 1:
             return 67;
+        case 2: {
+            Match *match = [self.bets[indexPath.row] match];
+            if (match.elapsed || match.finishedValue) {
+                return 363;
+            } else {
+                return 340;
+            }
+        }
         default:
             break;
     }
@@ -264,11 +394,12 @@
     self.tableView.dataSource = self;
     self.tableView.backgroundColor = self.view.backgroundColor;
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     [self.tableView registerClass:[ProfileTableViewCell class] forCellReuseIdentifier:@"ProfileCell"];
     [self.tableView registerClass:[WalletTableViewCell class] forCellReuseIdentifier:@"WalletCell"];
     [self.tableView registerClass:[WalletHighestTableViewCell class] forCellReuseIdentifier:@"WalletHighestCell"];
     [self.tableView registerClass:[ProfileChampionshipTableViewCell class] forCellReuseIdentifier:@"ChampionshipCell"];
+    [self.tableView registerClass:[MatchTableViewCell class] forCellReuseIdentifier:@"MatchCell"];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     [self.view addSubview:self.tableView];
     
