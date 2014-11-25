7b16a0bee2b73b55b449d282f50cfb045bd71a3a
Fernando Saragoca <fsaragoca@me.com>
2014-11-24 23:21:14 -0200

    Updates chat when coming from background

--- Footbl/GroupChatViewController.m
+++ Footbl/GroupChatViewController.m
@@ -519,6 +519,7 @@ static NSUInteger const kChatForceUpdateTimeInterval = 30;
     }
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(timerReloadData) name:UIApplicationDidBecomeActiveNotification object:nil];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
