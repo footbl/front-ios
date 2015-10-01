bd5f224bbe271c9f343eb38abd96e5286128d7fa
Leonardo Formaggio <leoformaggio@gmail.com>
2015-09-30 22:53:17 -0300

    Implements base layout for my challenge

---
+++ Footbl/Cells/Profile/MyChallengeCell.m
@@ -0,0 +1,23 @@
+//
+//  MyChallengeCell.m
+//  Footbl
+//
+//  Created by Leonardo Formaggio on 9/30/15.
+//  Copyright © 2015 Footbl. All rights reserved.
+//
+
+#import "MyChallengeCell.h"
+
+@implementation MyChallengeCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
