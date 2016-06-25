cab338fd84e09841c3b794bc515a5ea63638490e
Leonardo Formaggio <leoformaggio@gmail.com>
2016-06-25 11:05:22 -0300

    Fix challenge stake value color

--- Footbl/View Controllers/Challenges/ChallengesViewController.m
+++ Footbl/View Controllers/Challenges/ChallengesViewController.m
@@ -56,6 +56,7 @@
     }
 
     cell.stakeLabel.text = challenge.accepted ? challenge.valueString : challenge.match.finished ? @"-" : @"?";
+    cell.stakeLabel.textColor = challenge.match.finished ? [UIColor lightGrayColor] : [UIColor ftb_redStakeColor];
     cell.profitLabel.text = challenge.challengerResult == challenge.match.result ? challenge.valueString : @"-";
     cell.dateLabel.text = challenge.match.dateString;
     cell.guestTeamImageView.alpha = myResult == FTBMatchResultGuest ? 1 : 0.4;
